e747da4d60529db76b8afce815f6fae31086d7c2
Shahzaib Alam <shahxaibalam@gmail.com>
2020-06-07 02:16:19 +0500

    added verify

--- lib/src/pages/profile_page.dart
+++ lib/src/pages/profile_page.dart
@@ -133,7 +133,7 @@ class _ProfilePageState extends State<ProfilePage> {
                           fontSize: 16.0,
                         ),  
                       ),
-                      Icon(Icons.check_circle,
+                      if (userinfo['verified']) Icon(Icons.check_circle,
                       color: Colors.blue),
                       ]),
                       SizedBox(
