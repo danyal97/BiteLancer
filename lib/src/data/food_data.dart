a227cb319666f9fc09cd22a6be51875954d1d043
Syedplayer98 <k173605@nu.edu.pk>
2020-06-07 16:37:05 +0500

    static food items added

--- lib/src/data/food_data.dart
+++ lib/src/data/food_data.dart
@@ -3,7 +3,7 @@ import '../models/food_model.dart';
 final foods = [
   Food(
     id: "1",
-    name: "Hot Coffee",
+    name: "Haleem Caterers",
     imagePath: "assets/images/breakfast.jpeg",
     category: "1",
     price: 22.0,
@@ -12,7 +12,7 @@ final foods = [
   ),
   Food(
     id: "2",
-    name: "Grilled Chicken",
+    name: "Shahzaib caterers",
     imagePath: "assets/images/lunch.jpeg",
     category: "2",
     price: 12.0,
