22c3306ce29fcb3049e860809e74ee12f00bb7a4
danyal97 <41569341+danyal97@users.noreply.github.com>
2020-06-07 20:21:32 +0500

    dsanndsa

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,10 @@
+// import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:foodfreelancing/src/scoped-model/main_model.dart';
+import 'package:foodfreelancing/src/shared/loading.dart';
 import 'package:foodfreelancing/src/widgets/bought_foods.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:foodfreelancing/src/widgets/home_top_info.dart';
@@ -27,59 +31,103 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  // QuerySnapshot docs;
+
+  Future retrieveBuyerDocuments() async {
+    return await Firestore.instance.collection("BuyerRequest").getDocuments();
+
+    // print("Document Printing Called");
+    // // print(doc.documents.length);
+    // print("Document Printing Called");
+    // // var documents=document.documents;
+    // setState(() {
+    //   loading = false;
+    // });
+  }
+
+  Future retrieve() async {
+    var d = await retrieveBuyerDocuments();
+    print("dsdasdasdasd");
+    print(d.documents.length);
+  }
+
+  bool loading = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        padding: EdgeInsets.only(left: 20.0, right: 20.0),
-        children: <Widget>[
-          HomeTopInfo(),
-          FoodCategory(),
-          SizedBox(
-            height: 20.0,
-          ),
-          SearchField(),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                "Frequently Bought Foods",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
+    if (loading) {
+      retrieve();
+      return Loading();
+    } else {
+      return StreamBuilder<QuerySnapshot>(
+          // initialData: docs,
+          stream: Firestore.instance.collection('BuyerRequest').snapshots(),
+          builder: (context, snapshot) {
+            print("Document Length");
+            print(snapshot.data.documents.length);
+            print("Document Length");
+            if (snapshot.hasData && snapshot.data.documents.length > 0) {
+              snapshot.data.documents.forEach((document) {
+                print(document.data['username']);
+              });
+              return Scaffold(
+                backgroundColor: Colors.white,
+                body: ListView(
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                  children: <Widget>[
+                    HomeTopInfo(),
+                    FoodCategory(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SearchField(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Frequently Bought Foods",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            print("I' pressed");
+                          },
+                          child: Text(
+                            "View all",
+                            style: TextStyle(
+                              color: Colors.orangeAccent,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20.0),
+                    //ScopedModelDescendant<MainModel>(
+                    // builder: (BuildContext context, Widget child, MainModel model) {
+                    //return
+                    Column(
+                      children: _foods.map(_buildFoodItems).toList(),
+                    ),
+                    //},
+                    //),
+                  ],
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  print("I' pressed");
-                },
-                child: Text(
-                  "View all",
-                  style: TextStyle(
-                    color: Colors.orangeAccent,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 20.0),
-          //ScopedModelDescendant<MainModel>(
-          // builder: (BuildContext context, Widget child, MainModel model) {
-          //return
-          Column(
-            children: _foods.map(_buildFoodItems).toList(),
-          ),
-          //},
-          //),
-        ],
-      ),
-    );
+              );
+            } else {
+              return Container(
+                width: 0.0,
+                height: 0.0,
+              );
+            }
+          });
+    }
   }
 
   Widget _buildFoodItems(Food food) {
