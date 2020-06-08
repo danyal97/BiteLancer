585bb33a37a38dc7559ca6aff4cbb83648b0f827
Syedplayer98 <k173605@nu.edu.pk>
2020-06-09 03:59:22 +0500

    Login Fixed

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -23,26 +23,27 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.light,
-        backgroundColor: Colors.white,
-        // leading: IconButton(
-        //   onPressed: () {
-        //     Navigator.pop(context);
-        //   },
-        //   icon: Icon(
-        //     Icons.arrow_back_ios,
-        //     size: 20,
-        //     color: Colors.black,
-        //   ),
-        // ),
-      ),
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   brightness: Brightness.light,
+      //   backgroundColor: Colors.white,
+      //   // leading: IconButton(
+      //   //   onPressed: () {
+      //   //     Navigator.pop(context);
+      //   //   },
+      //   //   icon: Icon(
+      //   //     Icons.arrow_back_ios,
+      //   //     size: 20,
+      //   //     color: Colors.black,
+      //   //   ),
+      //   // ),
+      // ),
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Expanded(
               child: Column(
